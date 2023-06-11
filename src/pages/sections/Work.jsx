6581fc369d96a187c2d9490ac9bcea447e00a938
Alexandros Kangkelidis <64306207+alekosomegas@users.noreply.github.com>
2023-06-11 12:49:28 +0300

    fix: key

--- src/pages/sections/Work.jsx
+++ src/pages/sections/Work.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import Project from "../components/Project";
 import 'keen-slider/keen-slider.min.css'
 import { useKeenSlider } from 'keen-slider/react' // import from 'keen-slider/react.es' for to get an ES module
+import { nanoid } from "nanoid";
 
 const projectsData = require('@/projectsData')
 
@@ -43,7 +44,7 @@ const Work = React.forwardRef(function Work(props, ref) {
 
                     {projectsData.map((project,i) => {
                         return (
-                            <div className={`keen-slider__slide ${props.selectedSlide ? "h-screen w-screen" : ""}`}>
+                            <div key={nanoid()} className={`keen-slider__slide ${props.selectedSlide ? "h-screen w-screen" : ""}`}>
                                 <Project
                                     selectedSlide={props.selectedSlide}
                                     setSelectedSlide={props.setSelectedSlide}
