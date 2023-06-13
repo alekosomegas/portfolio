70a0c562346f18a2e5cb7c1943551f5e8ba738b3
Alexandros Kangkelidis <64306207+alekosomegas@users.noreply.github.com>
2023-06-13 14:32:56 +0300

    favicon

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -15,6 +15,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content={description} key="description" />
         <meta charSet="UTF-8" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </Head>
 
       <header>
